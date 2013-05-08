fb03a1e874dbc71df7e9a71937fdb850e9cd9b79
Stefan Sedich <stefan.sedich@gmail.com>
2013-05-09 08:10:31 +1000

    Cleanup

--- src/Rabbitus/RabbitusFactory.cs
+++ src/Rabbitus/RabbitusFactory.cs
@@ -5,13 +5,14 @@ namespace Rabbitus
 {
     public static class RabbitusFactory
     {
-        public static IRabbitus Configure(Action<IRabbitus> configure)
+        public static IRabbitus Configure(Action<RabbitusConfiguration> configure)
         {
             var configuration = new RabbitusConfiguration();
             var actorFactory = new DefaultActorFactory();
             var messageDispatcher = new MessageDispatcher(actorFactory);
             var rabbitus = new Rabbitus(configuration, messageDispatcher);
-            configure(rabbitus);
+
+            configure(configuration);
 
             return rabbitus;
         }
