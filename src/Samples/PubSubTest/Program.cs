4242b7f3b91041e56d76d5fa712b315d9795f030
Stefan Sedich <stefan.sedich@gmail.com>
2013-05-09 16:05:08 +1000

    Sample starts consuming at the end

--- src/Samples/PubSubTest/Program.cs
+++ src/Samples/PubSubTest/Program.cs
@@ -8,21 +8,19 @@ namespace PubSubTest
     {
         static void Main(string[] args)
         {
-            var rabbitus = RabbitusFactory.Configure(c => { });
+            var rabbitus = RabbitusFactory.Configure(c => c.Subscribe<SampleActor>());
 
-            //rabbitus.Subscribe<SampleActor>();
-            //rabbitus.Start();
+            Console.WriteLine("Will Start Publishing After Messages Are Published!!!");
+            Console.WriteLine("Enter {Message}...");
 
-            while (true)
-            {
-                Console.WriteLine("Enter {Message}...");
+            var line = Console.ReadLine();
 
-                var line = Console.ReadLine();
-                
-                Enumerable.Range(0, 1000000)
-                    .AsParallel()
-                    .ForAll(i => rabbitus.Publish(new SampleMessage { Message = line + " : " + i.ToString(), Timestamp = DateTime.Now }));
-            }
+            Enumerable.Range(0, 100000)
+                .AsParallel()
+                .ForAll(i => rabbitus.Publish(new SampleMessage { Message = line + " : " + i.ToString(), Timestamp = DateTime.Now }));
+
+            rabbitus.Start();
+            Console.ReadKey(false);
         }
     }
 }
